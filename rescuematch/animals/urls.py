539fa2a58540d831b800760cbadb9ce7d55bb0d3
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-05-04 13:58:09 +0100

    Implement dynamic animal browsing using Django templates and ORM with styled card layout

--- rescuematch/animals/urls.py
+++ rescuematch/animals/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('browse/', views.browse, name='browse'),
+    path('animal/<int:animal_id>/', views.animal_detail, name='animal_detail'),
 ]
--- rescuematch/animals/views.py
+++ rescuematch/animals/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Animal
 
 
@@ -9,3 +9,8 @@ def home(request):
 def browse(request):
     animals = Animal.objects.all()
     return render(request, "browse.html", {"animals": animals})
+
+
+def animal_detail(request, animal_id):
+    animal = get_object_or_404(Animal, id=animal_id)
+    return render(request, "animal_detail.html", {"animal": animal})
