0e13b052b2a6a5610c5c0932fa651a731a0a7596
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-05-25 19:51:11 +0100

    Add front end CRUD views for animals

--- rescuematch/animals/views.py
+++ rescuematch/animals/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Animal
+from .forms import AnimalForm
 
 
 def home(request):
@@ -37,5 +38,56 @@ def match_animal(request, animal_id):
         "next_animal": next_animal,
         "matched": True,
     })
+
+
+def manage_animals(request):
+    animals = Animal.objects.all()
+    return render(request, "manage_animals.html", {"animals": animals})
+
+
+def add_animal(request):
+    if request.method == "POST":
+        form = AnimalForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("manage_animals")
+    else:
+        form = AnimalForm()
+
+    return render(request, "animal_form.html", {
+        "form": form,
+        "page_title": "Add Animal",
+        "button_text": "Add Animal",
+    })
+
+
+def edit_animal(request, animal_id):
+    animal = get_object_or_404(Animal, id=animal_id)
+
+    if request.method == "POST":
+        form = AnimalForm(request.POST, request.FILES, instance=animal)
+        if form.is_valid():
+            form.save()
+            return redirect("manage_animals")
+    else:
+        form = AnimalForm(instance=animal)
+
+    return render(request, "animal_form.html", {
+        "form": form,
+        "page_title": "Edit Animal",
+        "button_text": "Save Changes",
+    })
+
+
+def delete_animal(request, animal_id):
+    animal = get_object_or_404(Animal, id=animal_id)
+
+    if request.method == "POST":
+        animal.delete()
+        return redirect("manage_animals")
+
+    return render(request, "delete_animal.html", {"animal": animal})
+
+
 def about(request):
     return render(request, "about.html")
