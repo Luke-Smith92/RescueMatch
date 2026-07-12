ac68a8759f1d105cc8ee683f057b129cab87c70d
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-07-12 16:02:50 +0100

    Amend assessment feedback - disable debug in production

--- rescuematch/rescuematch_project/settings.py
+++ rescuematch/rescuematch_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
