fc1af2c98fc561a114e19ebb91e009e3af2d298f
Luke-Smith92 <lsmith92@hotmail.co.uk>
2026-07-12 15:53:59 +0100

    Amend assessment feedback by hiding secret key and disabling debug

--- rescuematch/rescuematch_project/settings.py
+++ rescuematch/rescuematch_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oif&dsh)!6o+1pbjc@=jzv9q=yztg0s)h@1hv7=u)#3azfqc40'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-local-development-key-only",
+)
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
